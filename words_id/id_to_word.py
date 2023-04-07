f59d61e67edfdc1c2d658137bb3d396847dece54
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-04-07 14:17:17 +0200

    Update id_to_word.py

--- words_id/id_to_word.py
+++ words_id/id_to_word.py
@@ -4,6 +4,22 @@ path = "/data/mdehouck/thick_vectors/models"
 
 k = "4"
 
+
+
+id_to_word = {}
+word_to_id = {}
 with open(f"{path}/res_k_{k}_seed_0_fr_gsd.words", "r") as f:
     for line in f:
-        print(line.split(" "))
+
+        vals = line.split(" ")
+
+        if len(vals) != 2:
+            print("ERROR")
+            print(vals)
+        else:
+            if vals[1][-1] == "\n":
+                vals[1] = vals[1][:-1]
+
+
+            id_to_word[int(vals[1])] = vals[0]
+            word_to_id[vals[0]] = int(vals[1])
