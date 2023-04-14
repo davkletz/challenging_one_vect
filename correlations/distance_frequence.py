f4bae6020896226b603a777c22a740aa1e0d0468
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-04-14 12:53:10 +0200

    Update distance_frequence.py

--- correlations/distance_frequence.py
+++ correlations/distance_frequence.py
@@ -29,13 +29,6 @@ def eucl_distance(v_1, v_2):
     return np.linalg.norm(v_1 - v_2)
 def most_similars(vect_to_compare, list_vectors, n_k):
 
-    closests = []
-    distances = []
-
-
-    k = 0
-
-
     current_vect_to_compare = vect_to_compare
 
     list_similarities = []
@@ -51,8 +44,8 @@ def most_similars(vect_to_compare, list_vectors, n_k):
     indices = np.argsort(list_similarities)
 
 
-    indices = indices[:n_k].cpu().numpy()
-    sorted = sorted[:n_k].cpu().numpy()
+    indices = indices[:n_k]
+    sorted = sorted[:n_k]
 
 
 
