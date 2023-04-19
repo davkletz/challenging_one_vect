7d2da3ca1a57e2827b8a724b855c6b262813803c
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-04-19 11:10:18 +0200

    Update freq_UD.py

--- tools/freq_UD.py
+++ tools/freq_UD.py
@@ -10,7 +10,7 @@ def get_freq(path):
     data_file = open(path,"r", encoding="utf-8")
     for tokenlist in parse_incr(data_file):
         for token_avail in tokenlist:
-            print(token_avail['form'])
+            #print(token_avail['form'])
             if token_avail['form'] in dico:
                 dico[token_avail['form']] += 1
             else:
@@ -25,6 +25,9 @@ def get_freq(path):
 
 if __name__ == "__main__":
 
-    path = "/data/dkletz/data/UD/ud-treebanks-v2.11/UD_French-GSD/fr_gsd-ud-dev.conllu"
-    dico = get_freq(path)
+    path = "/data/dkletz/data/UD/ud-treebanks-v2.11"
+    corpus = "UD_French-GSD"
+    file = "fr_gsd-ud-dev.conllu"
+    dico = get_freq(f"{path}/{corpus}/{file}")
     print(dico)
+    dump(dico, f"dico_{corpus}_{file[:-7]}.joblib")
