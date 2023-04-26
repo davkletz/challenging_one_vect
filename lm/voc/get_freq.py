c72bdb09de77888bf1262d678333ee3046363234
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-04-26 18:37:39 +0200

    Update get_freq.py

--- lm/voc/get_freq.py
+++ lm/voc/get_freq.py
@@ -1,9 +1,30 @@
 
-
+from joblib import dump
 
 path = "/data/dkletz/Other_exp/AvecMatthieu/LSTM_ambiguity/language_model/"
 file = "training_corpus_word_frequencies.txt"
 
+
+results = {}
 with open(path + file, "rb") as f:
-    for line_available in f.readline():
+
+    for line_available in f.readlines():
         print(line_available)
+
+        content = line_available.split()
+        print(content)
+
+        if len(content) == 2:
+
+            word = str(content[0])
+            if word[-1] == ',':
+                word = word[:-1]
+            freq = int(content[1])
+            results[word] = freq
+
+
+
+dump(results, "word_freq.joblib")
+
+
+
