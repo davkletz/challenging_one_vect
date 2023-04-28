1ee2343897303e755b1eb7ba84cc126f47f075c7
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-04-28 12:01:22 +0200

    standardization

---
+++ lm/tools/centering_cluster.py
@@ -0,0 +1,58 @@
+from sklearn.cluster import KMeans
+from joblib import dump
+from torch import load, norm
+from joblib import load as ld
+
+
+def centering_cluster(X, n_clusters=1, random_state=0):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state, n_init="auto")
+
+    kmeans.fit(X)
+
+
+    return kmeans.cluster_centers_
+
+
+def get_vects(path, device):
+    """
+    Get the vectors from the language model
+    :param path: path to the language model
+    :param device: device to use
+    :return: the vectors
+    """
+    model = load(path, map_location=device)
+    return model.encoder.weight.data
+
+
+
+
+path = "/data/dkletz/Other_exp/AvecMatthieu/LSTM_ambiguity/language_model"
+file = "model.pt"
+
+model = load(f"{path}/{file}")
+
+
+embeddings = model.encoder.embedding
+
+
+centroid  = centering_cluster(embeddings, n_clusters=1, random_state=0)
+
+
+norms = norm(centroid, dim = 1).detach()
+
+norms = norms.detach().cpu().numpy()
+
+
+words_idx = ld(f"../voc/idx_to_word.joblib")
+words_freq = ld(f"../voc/word_freq.joblib")
+
+
+y = []
+for k in range(embeddings.weight.shape[0]):
+    word = words_idx[k]
+    #print(word)
+    y.append(norms[k])
+
+
+dump(y, path + "centroids_norms.joblib")
+
