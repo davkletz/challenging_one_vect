54a3ae5940dfd210bfbf3bdd8c76831cd1452db7
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-06-29 13:07:11 +0200

    Update get_norms.py

--- with_transformers/on_roberta/get_norms.py
+++ with_transformers/on_roberta/get_norms.py
@@ -12,10 +12,13 @@ b = list(a.keys())
 results = {}
 results_norms = []
 results_freq = []
+i = 0
 for element in b:
+    i+=1
+    if i%100 == 0:
+        print(i)
     current_list = a[element]
     tens = a[element][1]
-
     tot = sum(tens)
     results_norms.append(torch.norm(tens)/len(tens))
     results_freq.append(len(tens))
